c594b4f6f0625e8b8dfcb7530d049b1c178707d3
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-10-05 17:27:07 -0300

    Update code surfer version and first slides

--- src/greeting-loader.js
+++ src/greeting-loader.js
@@ -1,5 +1,19 @@
 import React from "react";
 
+import { useThemeUI } from "theme-ui";
+
+export function full(t) {
+  const { code } = t.styles.CodeSurfer;
+  return {
+    ...t,
+    colors: {
+      ...t.colors,
+      text: code.color,
+      background: code.backgroundColor,
+    },
+  };
+}
+
 const load = require.context("./", true, /greeting\..*js$/);
 
 export default function GreetingLoader({
@@ -9,15 +23,20 @@ export default function GreetingLoader({
   const Greeting = load("./greeting." + version + ".js")
     .default;
 
+  const { themeUi } = useThemeUI();
+
+  const t = theme || themeUi;
+
   return (
     <div
       style={{
         height: "100%",
+        width: "100%",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
         background:
-          theme && theme.codeSurfer.code.background,
+          t && t.styles.CodeSurfer.code.backgroundColor,
       }}
     >
       <Greeting name="Mary" />
