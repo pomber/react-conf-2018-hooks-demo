b585064f41daeaf254af10da772dd5c2148fb87d
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-10-07 17:32:11 -0300

    Update code-surfer version

--- src/greeting-loader.js
+++ src/greeting-loader.js
@@ -2,18 +2,6 @@ import React from "react";
 
 import { useThemeUI } from "theme-ui";
 
-export function full(t) {
-  const { code } = t.styles.CodeSurfer;
-  return {
-    ...t,
-    colors: {
-      ...t.colors,
-      text: code.color,
-      background: code.backgroundColor,
-    },
-  };
-}
-
 const load = require.context("./", true, /greeting\..*js$/);
 
 export default function GreetingLoader({
